0a7829eac2d357933402695c128896d3c59d7708
yao157 <116124739+yao157@users.noreply.github.com>
2023-01-03 15:44:12 +0200

    Update index.js

--- Scripts/index.js
+++ Scripts/index.js
@@ -5,8 +5,8 @@ const list = document.querySelector(".ajax-section .cities");
 const apiKey = "81bfcafd520cf95850fe973b68343bf3";
 
 form.addEventListener("submit", e => {
-    e.preventDefault();
-    let inputVal = input.value;
+  e.preventDefault();
+  let inputVal = input.value;
 
   const listItems = list.querySelectorAll(".ajax-section .city");
   const listItemsArray = Array.from(listItems);
@@ -16,63 +16,62 @@ form.addEventListener("submit", e => {
       let content = "";
       if (inputVal.includes(",")) {
         if (inputVal.split(",")[1].length > 2) {
-            inputVal = inputVal.split(",")[0];
-            content = el
-              .querySelector(".city-name span")
-              .textContent.toLowerCase();
-          } else {
-            content = el.querySelector(".city-name").dataset.name.toLowerCase();
-          }
+          inputVal = inputVal.split(",")[0];
+          content = el
+            .querySelector(".city-name span")
+            .textContent.toLowerCase();
         } else {
-            content = el.querySelector(".city-name span").textContent.toLowerCase();
+          content = el.querySelector(".city-name").dataset.name.toLowerCase();
         }
-        return content == inputVal.toLowerCase();
-      });
-  
-      if (filteredArray.length > 0) {
-        msg.textContent = `You already know the weather for ${
-          filteredArray[0].querySelector(".city-name span").textContent
-        } ...otherwise be more specific by providing the country code as well 😉`;
-        form.reset();
-        input.focus();
-        return;
+      } else {
+        content = el.querySelector(".city-name span").textContent.toLowerCase();
       }
+      return content == inputVal.toLowerCase();
+    });
+
+    if (filteredArray.length > 0) {
+      msg.textContent = `You already know the weather for ${
+        filteredArray[0].querySelector(".city-name span").textContent
+      } ...otherwise be more specific by providing the country code as well 😉`;
+      form.reset();
+      input.focus();
+      return;
     }
+  }
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        const { main, name, sys, weather } = data;
-        /*Recheck that*/
-        const icon = `./Icons/${
-          weather[0]["icon"]
-        }.svg`;
-  
-        const li = document.createElement("li");
-        li.classList.add("city");
-        const markup = `
-          <h2 class="city-name" data-name="${name},${sys.country}">
-            <span>${name}</span>
-            <sup>${sys.country}</sup>
-          </h2>
-          <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
-          <figure>
-            <img class="city-icon" src="${icon}" alt="${
-          weather[0]["description"]
-        }">
-            <figcaption>${weather[0]["description"]}</figcaption>
-          </figure>
-        `;
-        li.innerHTML = markup;
-        list.appendChild(li);
-      })
-      .catch(() => {
-        msg.textContent = "Please search for a valid city 😩";
-      });
-  
-    msg.textContent = "";
-    form.reset();
-    input.focus();
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const { main, name, sys, weather } = data;
+      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+        weather[0]["icon"]
+      }.svg`;
+
+      const li = document.createElement("li");
+      li.classList.add("city");
+      const markup = `
+        <h2 class="city-name" data-name="${name},${sys.country}">
+          <span>${name}</span>
+          <sup>${sys.country}</sup>
+        </h2>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <figure>
+          <img class="city-icon" src="${icon}" alt="${
+        weather[0]["description"]
+      }">
+          <figcaption>${weather[0]["description"]}</figcaption>
+        </figure>
+      `;
+      li.innerHTML = markup;
+      list.appendChild(li);
+    })
+    .catch(() => {
+      msg.textContent = "Please search for a valid city 😩";
+    });
+
+  msg.textContent = "";
+  form.reset();
+  input.focus();
 });
